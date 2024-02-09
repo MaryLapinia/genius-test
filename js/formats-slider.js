5498b914bb193c3f67b624eba78bbc28fcd11a28
Mary Lapinia <lapiniamariia30@gmail.com>
2024-02-10 00:32:20 +0200

    The end

--- js/formats-slider.js
+++ js/formats-slider.js
@@ -11,7 +11,9 @@ const swiper = new Swiper('.swiper-container', {
     },
   },
   pagination: {
-    el: '.swiper-pagination',
+    el: '.pagination',
+    bulletClass: 'pagination__button',
+    bulletActiveClass: 'pagination__button--active',
   },
   navigation: {
     nextEl: '.carousel-button.next',
