b1548cac4ffa1fea026a832061615cefa38689a7
Mary Lapinia <lapiniamariia30@gmail.com>
2024-02-11 23:47:47 +0200

    Add js

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -6,6 +6,21 @@ const toggleMenu = () => mobileMenu.classList.toggle('is-open');
 const disableScroll = () =>
   document.body.classList.toggle('is-scroll-disabled');
 
+/*const anchors = document.querySelectorAll('a[href*="#"]');
+
+anchors.forEach(anchor => {
+  anchor.addEventListener('click', event => {
+    event.preventDefault();
+
+    const blockID = anchor.getAttribute('href').substring(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  });
+});*/
+
 menuBtnOpen.addEventListener('click', toggleMenu);
 menuBtnClose.addEventListener('click', toggleMenu);
 
