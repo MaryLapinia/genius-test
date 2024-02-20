e7458bfbb9335f26c423e188d437212eb84c568d
Mary Lapinia <lapiniamariia30@gmail.com>
2024-02-20 20:21:56 +0200

    Now the modal window in the mobile version works))

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,16 @@
 const modal = document.querySelector('.backdrop');
-const modalBtnOpen = document.querySelector('.modal-btn-open');
-const modalBtnClose = document.querySelector('.modal-btn-close');
+const modalBtnOpen = document.querySelectorAll('.modal-btn-open');
+const modalBtnClose = document.querySelectorAll('.modal-btn-close');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
 
-modalBtnOpen.addEventListener('click', toggleModal);
-modalBtnClose.addEventListener('click', toggleModal);
+modalBtnOpen.forEach(function (e) {
+  e.addEventListener('click', toggleModal);
+});
+
+modalBtnClose.forEach(function (e) {
+  e.addEventListener('click', toggleModal);
+});
+
+/*modalBtnOpen.addEventListener('click', toggleModal);
+modalBtnClose.addEventListener('click', toggleModal);*/
